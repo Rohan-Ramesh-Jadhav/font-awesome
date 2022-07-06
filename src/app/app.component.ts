06f5e84bdbaf508ea0f1d607a19e0c1bbdd921b5
rohan <rohanjdhv550@gmail.com>
2022-07-06 16:10:10 +0530

    Icon usage in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { 
+			faCoffee, 
+		 	faAd,
+		 	faAdd,
+			faAddressBook,
+			faA,
+			faAddressCard,
+			faArrowAltCircleRight
+	   } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'font-awesome';
+  	title = 'font-awesome';
+	faA = faA;
+	faCoffee = faCoffee;
+	faAd = faAd;
+	faAdd = faAdd;
+	faAddressBook = faAddressBook;
+	faAddressCard = faAddressCard;
+	faArrowAltCircleRight = faArrowAltCircleRight;
 }
